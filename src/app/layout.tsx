3333d63ba5e11c3d7a283724432777cf245f155c
MohammedAdnan3071 <adnan160320733071@gmail.com>
2025-02-11 11:51:29 +0530

    added page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Inter } from "next/font/google";
+const inter = Inter ({ subsets:['latin']});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +26,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={cn(
+         'min-h-screen font-sans antialiased grainy', inter.className
+        )}>
         {children}
       </body>
     </html>
