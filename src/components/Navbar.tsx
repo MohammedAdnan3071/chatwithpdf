0214e6363864c7cbea9d78d08471f8e63e14baed
MohammedAdnan3071 <adnan160320733071@gmail.com>
2025-02-11 19:18:58 +0530

    space added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { buttonVariants } from "./ui/button";
 import {LoginLink, RegisterLink} from '@kinde-oss/kinde-auth-nextjs/server';
 const Navbar = () => {
+  
   return (
     <nav className= "sticky h-14 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
         <MaxWidthWarpper>
